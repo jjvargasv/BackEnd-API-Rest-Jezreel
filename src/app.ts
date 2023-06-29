1d84f62f63420aa4a8b2df145763299f4772ef08
Jhon Jairo Vargas Vargas <vargas.jhon@gmail.com>
2023-06-28 20:31:02 -0500

    [+] app.ts: define 2 EndPoints, / , /home

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,23 @@ import  express  from "express";
 
 const app = express();
 
+/** Routing: Enrutamiento */
+// http:localhost:3000/ (EndPoint)
+app.get( '/', (req, res) => { 
+
+    const message = 'Bienvenido a la API de Jezreel';
+    console.log(message);
+    res.send(`<h1>${ message} </h1>`);
+});
+
+// http:localhost:3000/home   (EndPoint)
+app.get( '/home', (req, res) => { 
+
+    const namePage = 'home Jezreel';
+    console.log(namePage);
+    res.send(`<h1>${ namePage} </h1>`);
+});
+
 app.listen(3000,()=>{
     console.log('servidor lanzado en el puerto 3000');
 });
