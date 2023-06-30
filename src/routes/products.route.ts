51bc3a8308dd0db3733500fb3fea3df8b064a8d3
Jhon Jairo Vargas Vargas <vargas.jhon@gmail.com>
2023-06-29 21:48:03 -0500

    [+] vinculamos rutas de productos al controlador

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -1,11 +1,14 @@
 /* archivo central de enrutamieno */
 import { Router, Request, Response } from "express";
-import { getProducts } from "../controllers/product.controller";
+import { createProducts, deleteProducts, getProduct, getProducts, updateProducts } from "../controllers/product.controller";
 
 
 const router =Router();
-
+// http://localhost:3000/api/products
 router.get('/', getProducts);
-
+router.get( '/',getProduct);
+router.post( '/', createProducts);
+router.put( '/', updateProducts);
+router.delete( '/', deleteProducts);
 
 export default router;
