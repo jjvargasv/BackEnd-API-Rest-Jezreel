d9f8fa064c634c8d1ee7afcf7c8536e87ab33966
Jhon Jairo Vargas Vargas <vargas.jhon@gmail.com>
2023-07-11 21:48:37 -0500

    [+]restringir las rutas a productos y se crea la ruta /user/id

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -5,12 +5,16 @@ import { authenticationUser } from "../middleware/authentication.middleware";
 
 const router = Router();
 // http://localhost:3000/api/products/
-router.get( '/',authenticationUser, getProducts );                      // obtiene lista de todos lo productos
-router.get( '/:id', getProduct );                    // obtener producto para ID
-router.post( '/', createProduct );                   // obtener productos
-router.put( '/:id', updateProduct );                 // actualizar todo el producto
-router.patch( '/:id', parcialUpdateProduct );        // actualizar algunos productos
-router.delete( '/:id', deleteProduct );              // elimina todo el producto
-
+router.get( '/', getProducts );                         // obtiene lista de todos lo productos
+router.get( '/:id', getProduct );                       // obtener producto para ID
+router.post( '/', authenticationUser, createProduct );   // obtener productos
+router.put( '/:id', authenticationUser, updateProduct );                    // actualizar todo el producto
+router.patch( '/:id', authenticationUser, parcialUpdateProduct );           // actualizar algunos productos
+router.delete( '/:id', authenticationUser, deleteProduct );                 // elimina todo el producto
+router.get( '/user/:id',authenticationUser, ( req: Request, res: Response)=>{
+    res.json( {
+        msg:'TODO Obtener todos lo productos del usuario'
+    });
+} );                                        
 
 export default router;
